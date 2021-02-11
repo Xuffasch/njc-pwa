853150ceeeffa32fd732f43a01493b90b663bfc1
Xuffasch <tristan.lucas@gmail.com>
2021-02-11 19:42:33 +0100

    remove Typescript

---
+++ pages/checkoutKO.js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+export default function CheckoutOK() {
+    const router = useRouter();
+
+    const returnQuery = router.query;
+
+    return ( 
+        <>
+            <h1>Stripe Checkout KO</h1>
+        </>
+    )
+
+}
--- utils/get-stripe.js
+++ utils/get-stripe.js
@@ -1,6 +1,6 @@
 import { Stripe, loadStripe } from '@stripe/stripe-js';
 
-let stripePromise: Promise<Stripe|null>;
+let stripePromise;
 export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
